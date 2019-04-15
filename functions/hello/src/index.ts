0f8df4c632dc603b3ad0f9462465462ae2ef1ea8
HubCodes <krlrhkstk12@gmail.com>
2019-04-15 13:52:33 +0900

    AWS Credential 다시 세팅

--- functions/hello/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Handler } from "aws-lambda";
-
-const handler: Handler = (e, _, cb) => {
-  console.log("processing event: %j", e);
-  cb(null, { hello: "world" });
-};
-
-export default handler;
