d36fec4eeb65e532af4cae381012f37cbf4fba58
HubCodes <krlrhkstk12@gmail.com>
2019-04-23 11:30:26 +0900

    registerMyApartment: 세대주 id 반환하도록 변경

--- functions/registerMyApartment/src/index.ts
+++ functions/registerMyApartment/src/index.ts
@@ -4,18 +4,21 @@ import * as uuid from "uuid/v4";
 import { docClient } from "../../../aws";
 import { ApartmentInformation } from "../../../types/ApartmentInformation";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { DYNAMODB_COMFORTMENT_AI } from "../../../constants";
 
 const handler: Handler = async (event: ApartmentInformation, _, __) => {
   const id = uuid();
-  const input: DocumentClient.PutItemInput = {
-    TableName: "ComfortmentAI",
+  const dynamodbPutData: DocumentClient.PutItemInput = {
+    TableName: DYNAMODB_COMFORTMENT_AI,
     Item: {
       id,
       ...event
     }
   };
 
-  await docClient.put(input).promise();
+  await docClient.put(dynamodbPutData).promise();
+
+  return id;
 };
 
 export default handler;
