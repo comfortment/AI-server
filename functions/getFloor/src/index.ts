af50b2c0b027736752b9b91e3f8c5494b3338675
HubCodes <krlrhkstk12@gmail.com>
2019-04-29 13:39:09 +0900

    getFloor: 층 단위로 데이터를 가져오는 함수

---
+++ functions/getFloor/src/index.ts
@@ -0,0 +1,9 @@
+import { Handler } from "aws-lambda";
+
+import { GetFloorRequest } from "../../../types/GetFloorRequest";
+
+const handler: Handler = async (event: GetFloorRequest, _, __) => {
+  return;
+};
+
+export default handler;
---
+++ types/GetFloorRequest.ts
@@ -0,0 +1,4 @@
+export interface GetFloorRequest {
+  buildingNumber: number;
+  floor: number;
+}
