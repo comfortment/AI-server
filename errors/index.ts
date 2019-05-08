736a78b60aea4c08443514b1c951693f585d6c8a
HubCodes <krlrhkstk12@gmail.com>
2019-05-08 22:24:19 +0900

    getFloor: AWS API Gateway proxy integration

--- errors/index.ts
+++ errors/index.ts
@@ -1,2 +1,3 @@
 export const APARTMENT_NOT_FOUND = new Error("세대 정보를 찾지 못했습니다.");
 export const BUILDING_NOT_FOUND = new Error("아파트 동 정보를 찾지 못했습니다.");
+export const FLOOR_NOT_FOUND = new Error("아파트 층 정보를 찾지 못했습니다.");
--- functions/getFloor/src/index.ts
+++ functions/getFloor/src/index.ts
@@ -1,9 +1,67 @@
-import { Handler } from "aws-lambda";
+import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import { GetFloorRequest } from "../../../types/GetFloorRequest";
+import { docClient } from "../../../aws";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import {
+  DYNAMODB_COMFORTMENT_AI,
+  DYNAMODB_COMFORTMENT_AI_BY_BUILDING_NUMBER
+} from "../../../constants";
+import { FLOOR_NOT_FOUND } from "../../../errors";
+import { ApartmentInformation } from "../../../types/ApartmentInformation";
 
-const handler: Handler = async (event: GetFloorRequest, _, __) => {
-  return;
+const floorToApartmentRoomNumber = (floor: number): number => {
+  return floor * 100;
+};
+
+const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  _,
+  __
+): Promise<APIGatewayProxyResult> => {
+  const { buildingNumber: buildingNumberString, floor: floorString } = event.queryStringParameters;
+  if (!buildingNumberString || !floorString) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Malformed request parameter."
+      })
+    };
+  }
+
+  const buildingNumber = Number(buildingNumberString),
+    floor = Number(floorString);
+
+  const dynamodbGetDataQuery: DocumentClient.QueryInput = {
+    TableName: DYNAMODB_COMFORTMENT_AI,
+    IndexName: DYNAMODB_COMFORTMENT_AI_BY_BUILDING_NUMBER,
+    KeyConditionExpression: "buildingNumber = :buildingNumber",
+    ExpressionAttributeValues: {
+      ":buildingNumber": buildingNumber
+    }
+  };
+
+  const { Items } = await docClient.query(dynamodbGetDataQuery).promise();
+
+  if (!Items || !Items.length) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: FLOOR_NOT_FOUND.message
+      })
+    };
+  }
+
+  const apartmentsInFloor = Items.filter(({ roomNumber }: ApartmentInformation) => {
+    const begin = floorToApartmentRoomNumber(floor);
+    const end = floorToApartmentRoomNumber(floor + 1);
+
+    return begin <= roomNumber && roomNumber < end;
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(apartmentsInFloor)
+  };
 };
 
 export default handler;
