6e2b0afe5aba3c46ddd0c4e0d48af99beab94691
HubCodes <krlrhkstk12@gmail.com>
2019-05-08 17:13:52 +0900

    registerMyApartment: API Gateway proxy integration

--- functions/registerMyApartment/src/index.ts
+++ functions/registerMyApartment/src/index.ts
@@ -1,20 +1,40 @@
-import { Handler } from "aws-lambda";
+import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import * as uuid from "uuid/v4";
 
 import { docClient } from "../../../aws";
 import { ApartmentInformation } from "../../../types/ApartmentInformation";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { DYNAMODB_COMFORTMENT_AI } from "../../../constants";
+import { AWSError } from "aws-sdk";
 
-const handler: Handler = async (event: ApartmentInformation, _, __) => {
+const handler: Handler = async (
+  event: APIGatewayProxyEvent,
+  _,
+  __
+): Promise<APIGatewayProxyResult> => {
+  const body = JSON.parse(event.body) as ApartmentInformation;
   const dynamodbPutData: DocumentClient.PutItemInput = {
     TableName: DYNAMODB_COMFORTMENT_AI,
     Item: {
-      ...event
+      ...body
     }
   };
 
-  await docClient.put(dynamodbPutData).promise();
+  try {
+    await docClient.put(dynamodbPutData).promise();
+  } catch {
+    return {
+      statusCode: 400,
+      body: ""
+    };
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "success"
+    })
+  };
 };
 
 export default handler;
